9f8a987251f912ac39e093c85b1f891bae737102
lucasquin <lucaslquintino@gmail.com>
2026-01-16 11:35:55 -0300

    feat: adiciona sinalizacao visual para a versao de homologacao

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,15 @@
-import { useAppState } from "@hooks/state";
-import { Outlet } from "react-router-dom";
-import Menu from "../Menu";
+import { useAppState } from '@hooks/state';
+import { Outlet } from 'react-router-dom';
+import Menu from '../Menu';
 
 export function Layout() {
   const { state } = useAppState();
+  const isHomolog = import.meta.env.VITE_API_UPLOAD_URL?.includes("homolog");
+    console.log('VITE_API_UPLOAD_URL:', import.meta.env.VITE_API_UPLOAD_URL);
+
   return state?.auth?.token?.length > 0 ? (
     <div className="flex flex-col h-full w-full">
+      {isHomolog ? <p className="w-full bg-red-500 text-center font-bold text-white">VERSÃO DE HOMOLOGAÇÃO</p> : <></>}
       <Menu />
       <div className="flex-1 min-h-0">
         <Outlet />
